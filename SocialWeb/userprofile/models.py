efe3d13183f978594e52e328fa5f2fd489a97af1
near77 <31362822+near77@users.noreply.github.com>
2018-06-22 19:21:27 +0800

    DB

--- SocialWeb/userprofile/models.py
+++ SocialWeb/userprofile/models.py
@@ -9,8 +9,8 @@ class Profile(models.Model):
     about_user = models.TextField(default = " ")
     city = models.CharField(default = '',max_length = 50)
     birthday = models.CharField(default = '',max_length = 50)
-    follower = models.ManyToManyField(User,null = True, blank = True,related_name='%(class)s_follower')
-    follow = models.ManyToManyField(User,null = True, blank = True,related_name='%(class)s_follow')
+    follower = models.ManyToManyField(User, blank = True,related_name='%(class)s_follower')
+    follow = models.ManyToManyField(User, blank = True,related_name='%(class)s_follow')
     
     def get_absolute_url(self):
         return reverse('userprofile:personal_page',kwargs = {'pk':self.author.id})
